96c427b74ecd16f299c900ac6b9e76c4be2b69a3
InfernumVIta <martisbanis@gmail.com>
2025-05-03 20:26:01 +0300

    Update main.h

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -1,22 +1,5 @@
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  * @Author         : Lekt. dr. Vytautas Daunroas
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
